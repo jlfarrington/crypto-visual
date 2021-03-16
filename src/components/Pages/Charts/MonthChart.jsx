e387715c8653d1993b9b33f53253cc3e535e2b3d
Tristanoshier <tsoshier@bsu.edu>
2021-03-16 19:57:47 -0400

    work on month chart BTC

--- src/components/Pages/Charts/MonthChart.jsx
+++ src/components/Pages/Charts/MonthChart.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
+import { LineChart } from '../../Shared/LineChart';
+
 export const MonthChart = () => {
+    const [data, setData] = useState();
+    const [fetchingData, setFetchingData] = useState(true);
+
+    useEffect(() => {
+        fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
+            .then(response => response.json())
+            .then(data => {
+                setData(data.bpi);
+                setFetchingData(false);
+            })
+            .catch(error => console.log(error));
+    }, [])
+
     return (
-        <div>
-            <h1>This is the month chart</h1>
-        </div>
+        <>
+            {!fetchingData ? <LineChart data={data}/>: null}
+        </>
     )
 }
