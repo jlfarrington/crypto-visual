23cfcb582a6bc2ea815cfe4c327f32ad1e39f3f8
Tristanoshier <tsoshier@bsu.edu>
2021-03-22 17:23:14 -0400

    start styling custom component

--- src/components/Pages/Charts/CustomChart.jsx
+++ src/components/Pages/Charts/CustomChart.jsx
@@ -7,48 +7,55 @@ export const CustomChart = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [customPrices, setCustomPrices] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const customURL = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`;
 
   const getCustomData = async () => {
+    setCustomData({});
+    setIsLoading(true);
     const response = await fetch(customURL)
-    .then(data => data.json())
-    .then(bitcoinData => {
-      setCustomData(bitcoinData.bpi);
-      setCustomPrices(Object.values(bitcoinData.bpi))
-    })
-    .catch(() => alert('please enter accurate date'));
+      .then(data => data.json())
+      .then(bitcoinData => {
+        setCustomData(bitcoinData.bpi);
+        setCustomPrices(Object.values(bitcoinData.bpi));
+        setIsLoading(false);
+      })
+      .catch(() => {
+        alert('please enter accurate date');
+        setIsLoading(false);
+      });
     return response;
-
   };
 
   return (
     <div className='crypto-page'>
-    <h2>Select Start and End Dates to View Bitcoin Data:</h2>
-      <div className="custom-date-input">
-        
-        <label htmlFor='start-date'>Start date:</label>
-        <input
-          type='date'
-          id='start-date'
-          onChange={(e) => setStartDate(e.target.value)}
-        />
-        <label htmlFor='end-date'>End date:</label>
-        <input
-          type='date'
-          id='end-date'
-          onChange={(e) => setEndDate(e.target.value)}
-        />
-        <button onClick={() => getCustomData()}>Submit</button>
+      <div className='calc dates'>
+        <h2 className='calc-text'>Select Start and End Dates to View Bitcoin Data</h2>
+        <div className='custom-date-input'>
+          <label htmlFor='start-date'>Start date:</label>
+          <input
+            type='date'
+            id='startdate'
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+          <label htmlFor='end-date'>End date:</label>
+          <input
+            type='date'
+            id='enddate'
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+        </div>
+        <button className='submit-btn' onClick={() => getCustomData()}>Submit</button>
       </div>
-
-      {customData ? (
+      
+      {customData || !isLoading ? (
         <div>
           <div>
             <LineChart data={customData} />
           </div>
-            <CustomCalculation startDate={startDate} endDate={endDate} prices={customPrices} />
+          <CustomCalculation startDate={startDate} endDate={endDate} prices={customPrices} />
         </div>
-      ) : <></>}
+      ) : <div className="loader"></div>}
     </div>
   );
 };
